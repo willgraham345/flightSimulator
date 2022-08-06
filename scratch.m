6721eedf31ea04bc78734e3e02711c84be5a16e4
Will Desktop - Home <willgraham345@gmail.com>
2022-08-05 19:08:28 -0600

    Free body dynamics are now finally working
    Rotation matrix was reverse what it should have been ("ZYX" instead of "XYZ" due to a Simulink default)

--- scratch.m
+++ scratch.m
@@ -2,16 +2,20 @@
 close all; clear; clc; format compact;
 
 %% Make rot matrix
-phi = 0
-theta = 0
-psi = deg2rad(90)
-R = eul2rotm([phi, theta, psi], "ZYX")
+% eul = [phi, theta, psi]
+% stuff = eul2quat(eul, "ZYX")
+
+%%
+% R = eul2rotm([psi, theta, psi], 'ZYX');
+R = angle2dcm(0, 0, pi/2, "XYZ")
 R_inv = inv(R)
 
 
-vX_b = 0,; vY_b = 0; vZ_b = 1;
+vX_b = 0;
+vY_b = 0; 
+vZ_b = 1;
 
-v_B = [vX_b, vY_b, vZ_b]';
+v_B = [vX_b, vY_b, vZ_b]'
 
 
 v_A = R_inv*v_B
