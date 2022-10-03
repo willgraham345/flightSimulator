a3ecf4363acca79acb152bca20c7be0276676609
Will <willgraham345@gmail.com>
2022-10-03 13:33:56 -0600

    Started playing w/Quaternions, not too much progress.
    Still learning.

--- scratch.m
+++ scratch.m
@@ -14,21 +14,37 @@
 % view(-30,45)
 
 %%
-clear; clc; close all; format compact;
-ld = load('rpy_9axis.mat')
-accel = ld.sensorData.Acceleration;
-gyro = ld.sensorData.AngularVelocity;    
-Fs  = ld.Fs;
-decim = 2;
-fuse = imufilter('SampleRate',Fs,'DecimationFactor',decim);
-pose = fuse(accel,gyro);
+% clear; clc; close all; format compact;
+% ld = load('rpy_9axis.mat')
+% accel = ld.sensorData.Acceleration;
+% gyro = ld.sensorData.AngularVelocity;    
+% Fs  = ld.Fs;
+% decim = 2;
+% fuse = imufilter('SampleRate',Fs,'DecimationFactor',decim);
+% pose = fuse(accel,gyro);
+% 
+% tp = theaterPlot('XLimit',[-2 2],'YLimit',[-2 2],'ZLimit',[-2 2]);
+% op = orientationPlotter(tp,'DisplayName','Fused Data',...
+%     'LocalAxesLength',2);
+% 
+% 
+% for i=1:numel(pose)
+%     plotOrientation(op, pose(i))
+%     drawnow
+% end
+% 
 
-tp = theaterPlot('XLimit',[-2 2],'YLimit',[-2 2],'ZLimit',[-2 2]);
-op = orientationPlotter(tp,'DisplayName','Fused Data',...
-    'LocalAxesLength',2);
+%% Quaternion garbage
+clear; close all; format compact; clc;
+A = rotz(45);
 
+eul = [0 pi/2 0;]
+q = [1 0 1 0];
+r = [1 1 1];
+n = quatrotate(q, r);
 
-for i=1:numel(pose)
-    plotOrientation(op, pose(i))
-    drawnow
-end
+eul = [0 pi/2 0];
+qZYX = eul2quat(eul)
+qZYX_1 = quatconj(qZYX)
+
+n1 = qZYX*r
